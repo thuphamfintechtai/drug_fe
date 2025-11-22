fbfe210c39148de24858e25d45bf5f804662ccf4
pham thi anh thu <phamthianhthu@MacBook-Air-cua-pham.local>
2025-11-22 23:32:05 +0700

    fix format

--- src/features/admin/hooks/useNFT-Tracking.js
+++ src/features/admin/hooks/useNFT-Tracking.js
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from "react";
-import api from "../../../utils/api";
+import api from "../../utils/api";
 
 export default function useNFTTracking() {
   const [nftId, setNftId] = useState("");
